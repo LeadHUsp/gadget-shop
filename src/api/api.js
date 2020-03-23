37ccd78e5504cdf930fec3b97899d38a79dc55fc
LeadHUsp <bodvinukr@gmail.com>
2020-03-23 18:17:05 +0300

    fix url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:1337/"
+  baseURL: "https://pacific-depths-36780.herokuapp.com/"
 });
 
 export const NavigationApi = {
